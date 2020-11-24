21bdacf5e1db06086deb544e2f74294452b2dd45
Isaac Arnold <isaac@iaivphotography.com>
2020-11-24 13:17:52 -0600

    links work.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,15 +6,20 @@ import Navbar from "./components/Navbar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Portfolio from './pages/Portfolio';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/portfolio" component={Portfolio} />
       {/* <Home/> */}
       {/* <Contact/> */}
-      <Portfolio/>
-      
+      {/* <Portfolio/> */}
+      </Router>
     </div>
   );
 }
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Home from "../pages/Home"
+import Contact from '../pages/Contact';
+import Portfolio from '../pages/Portfolio';
+import { Link } from "react-router-dom";
+
+
+
 
 
 function Navbar(props) {
@@ -7,15 +14,15 @@ function Navbar(props) {
         <>
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="index.html">Isaac Arnold</a>
+            <Link className="navbar-brand" to="/"> Isaac Arnold </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="index.html">About Me <span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link" href="contact.html">Contact</a>
-                        <a className="nav-item nav-link" href="portfolio.html">Portfolio</a>
+                    <Link className="navbar-brand" to="/"> About Me </Link>
+                    <Link className="navbar-brand" to="/contact"> Contact </Link>
+                    <Link className="navbar-brand" to="/portfolio"> Portfolio </Link>
                     </div>
                 </div>
             </nav>
